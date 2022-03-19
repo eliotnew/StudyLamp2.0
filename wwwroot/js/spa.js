6ec2da87083273e3afb1bef3863fab8263c2e0a2
Eliot New <eliot.new@students.plymouth.ac.uk>
2022-03-19 19:24:33 +0000

    Now added some working page transitions as well as working home

--- wwwroot/js/spa.js
+++ wwwroot/js/spa.js
@@ -3,4 +3,24 @@
     $('#about').click(function () {
         $('#page-content-wrapper').load('about.html');
     })
-});
+});$(document).ready(function () {
+
+    $('#startStudy').click(function () {
+        $('#page-content-wrapper').load('startstudy.html');
+    })
+});$(document).ready(function () {
+
+    $('#viewStats').click(function () {
+        $('#page-content-wrapper').load('viewstats.html');
+    })
+});$(document).ready(function () {
+
+    $('#about').click(function () {
+        $('#page-content-wrapper').load('about.html');
+    })
+});$(document).ready(function () {
+
+    $('#home').click(function () {
+        $('#page-content-wrapper').load('homescreen.html');
+    })
+});
